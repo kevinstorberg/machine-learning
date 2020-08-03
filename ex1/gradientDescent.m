12b42c07ce5443b7ecfb04fab6662ccae160fba0
Kevin J. Storberg <kevin@storberg.net>
2020-08-02 22:27:44 -0700

    part 3, gradient decent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -8,7 +8,7 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
+    theta = theta - ((alpha / m) * ((X * theta) - y)' * X)';
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
