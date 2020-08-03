748de9ffe5c9ef97a84603a52a5e1b9f4d1d499f
Kevin J. Storberg <kevin@storberg.net>
2020-08-02 21:33:23 -0700

    simple linear regression cost function

--- ex1/plotData.m
+++ ex1/plotData.m
@@ -2,7 +2,9 @@ function plotData(x, y)
 %PLOTDATA Plots the data points x and y into a new figure 
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
 figure; % open a new figure window
 
 % ====================== YOUR CODE HERE ======================
